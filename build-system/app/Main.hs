34c82012eb9bddf977bdd6c0f985f29250cbea2a
Sandi <dsandi333@gmail.com>
2019-12-20 20:35:47 +0100

    add docker command example

--- build-system/app/Main.hs
+++ build-system/app/Main.hs
@@ -5,5 +5,6 @@ import Development.Shake
 main :: IO ()
 main = shakeArgs shakeOptions $ do
     phony "build" $ do
-        command_ [Cwd "../my-app"] "stack" ["build"]
+        command_ [Cwd "my-app/"] "stack" ["build"]
+        -- command_ [] "docker" ["run", "--rm", "alpine", "ls", "/bin"]
         putQuiet "done building"
