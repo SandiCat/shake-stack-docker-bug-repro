e33454dcc028cecdd6b6c15a3b2426089f3cab33
Sandi <dsandi333@gmail.com>
2019-12-20 20:56:27 +0100

    update

--- build-system/app/Main.hs
+++ build-system/app/Main.hs
@@ -5,6 +5,6 @@ import Development.Shake
 main :: IO ()
 main = shakeArgs shakeOptions $ do
     phony "build" $ do
-        command_ [Cwd "my-app/"] "stack" ["build"]
+        command_ [Cwd "../my-app/"] "stack" ["-v", "build"]
         -- command_ [] "docker" ["run", "--rm", "alpine", "ls", "/bin"]
         putQuiet "done building"
